0ab080cdfaa10aa0b6c531d5403583dec561508a
josepht273 <josephtadessem@gmail.com>
2026-02-20 23:16:26 +0300

    Chat app

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import index from "./index.html";
+
+
+const server = Bun.serve({
+  port: 3000,
+  routes: {
+    "/*": index
+  },
+
+  fetch(req, server) {
+    if (server.upgrade(req)) {
+      return;
+    }
+    return new Response("Internal Server error", { status: 500 });
+  },
+
+  websocket: {
+    open(ws) {
+      const sessionID = Math.random().toString(36).slice(2, 10);
+      ws.subscribe("chat")
+      ws.send(`[SERVER] Connected. Session: ${sessionID}`);
+    },
+
+    message(ws, message) {
+      server.publish("chat", `[${new Date().toLocaleTimeString()}] ${message}`);
+    },
+
+    close(ws, code, reason) {
+      console.log("Closed", code, reason);
+    },
+  },
+});
+
+console.log(`Server running → ws://localhost:${server.port}`);
